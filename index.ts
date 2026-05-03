a52d046b69e1b9034d03c942e5b54021e046db83
totny <totny97@gmail.com>
2026-05-03 16:19:48 +0800

    feat: 根据url返回不同文件

--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
 import * as http from 'http';
+import * as fs from 'fs';
+import * as path from 'path';
 
+const publicDir = path.resolve(__dirname, './public');
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  console.log('feng 有人请求了');
-
-  response.end('hi 来自服务器');
+  const { method, url, headers } = request;
+  switch (url) {
+    case '/index.html':
+      fs.readFile(path.resolve(publicDir, './index.html'), (error, data) => {
+        if (error) throw error;
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      fs.readFile(path.resolve(publicDir, './style.css'), (error, data) => {
+        if (error) throw error;
+        response.setHeader('Content-Type', 'text/css; charset=utf-8');
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      fs.readFile(path.resolve(publicDir, './main.js'), (error, data) => {
+        if (error) throw error;
+        response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+        response.end(data.toString());
+      });
+      break;
+  }
 });
 
 server.listen(9999);
---
+++ public/main.js
@@ -0,0 +1,4 @@
+const h1El = document.querySelector('h1');
+h1El?.addEventListener('click', () => {
+  alert('点击');
+});
