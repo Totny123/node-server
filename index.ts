910e5a5f7adc997360f4bbb2fd6c97a671febeb0
totny <totny97@gmail.com>
2026-05-04 14:31:28 +0800

    feat: 静态服务器

--- index.ts
+++ index.ts
@@ -9,33 +9,28 @@ const server = http.createServer();
 server.on('request', (request, response) => {
   const { method, headers } = request;
   const { pathname } = new URL(request.url || '', 'http://localhost');
+  const fileName = pathname.slice(1) || 'index.html';
 
-  switch (pathname) {
-    case '/index.html':
-      fs.readFile(path.resolve(publicDir, './index.html'), (error, data) => {
-        if (error) throw error;
-        response.setHeader('Content-Type', 'text/html; charset=utf-8');
-        response.end(data.toString());
-      });
-      break;
-    case '/style.css':
-      fs.readFile(path.resolve(publicDir, './style.css'), (error, data) => {
-        if (error) throw error;
-        response.setHeader('Content-Type', 'text/css; charset=utf-8');
-        response.end(data.toString());
-      });
-      break;
-    case '/main.js':
-      fs.readFile(path.resolve(publicDir, './main.js'), (error, data) => {
-        if (error) throw error;
-        response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
-        response.end(data.toString());
-      });
-      break;
-    default:
-      response.statusCode = 404;
-      response.end();
-  }
+  fs.readFile(path.resolve(publicDir, fileName), (error, data) => {
+    if (error) {
+      console.log('feng error', error);
+      if (error.errno === -2) {
+        response.statusCode = 404;
+        fs.readFile(path.resolve(publicDir, '404.html'), (e, d) => {
+          if (e) throw e;
+          response.end(d);
+        });
+      } else if (error.errno === -21) {
+        response.statusCode = 403;
+        response.end('无权限');
+      } else {
+        response.statusCode = 500;
+        response.end('服务器繁忙');
+      }
+    } else {
+      response.end(data);
+    }
+  });
 });
 
 server.listen(9999);
---
+++ public/jq.js
@@ -0,0 +1,1 @@
+console.log('feng jq');
